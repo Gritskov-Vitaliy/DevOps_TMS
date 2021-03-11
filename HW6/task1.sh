2217d81f1f08e9fd4e6c05d5ac349ce1a28cc04c
Gritskov Vitaliy <Gritskov_Vitaliy@outlook.com>
2021-03-11 09:51:54 +0000

    Edit HW6

--- HW6/task1.sh
+++ HW6/task1.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+
+log_file="/var/log/custom_daemon.log"
+
 while :;
 do
-log_file="/var/log/custom_daemon.log"
-date +"%S:%M:%H %d-%m-%Y" >> $log_file
-echo '     total free' >> $log_file
-free -hm | grep Mem | awk '{print $1, $2, $4}' >> $log_file
-echo HDD: `df -h | awk '{if ($6 == "/") { print $2, $4 }}' | head -1` >> $log_file
-echo CPU: `cat /proc/cpuinfo | grep "model name" -m1 | cut -c14-` >> $log_file
-uptime | awk '{print $6, $7, $8, $9, $10}' >> $log_file
+DATA=`date +"%S:%M:%H %d-%m-%Y"`
+MEM=`free -hm | grep Mem | awk '{print $1, $2, $4}'`
+HDD="HDD: `df -h | awk '{if ($6 == "/") { print $2, $4 }}' | head -1`"
+CPU="CPU: `cat /proc/cpuinfo | grep "model name" -m1 | cut -c14-`"
+Load_average=`uptime | awk '{print $6, $7, $8, $9, $10}'`
+echo "$DATA, $MEM, $HDD, $CPU, $Load_average" >> $log_file
 echo "----------------------------------" >> $log_file
 sleep 30
 done
