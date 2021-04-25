41e91bbfb8fcb81ff2ce31da3d4c16d5a2ea9e34
Gritskov Vitaliy <Gritskov_Vitaliy@outlook.com>
2021-04-25 13:00:51 +0000

    HW16, replaced with functions in script.sh

--- HW16/script.sh
+++ HW16/script.sh
@@ -4,42 +4,43 @@ A=$1
 B=$2
 C=$3
 
+function A_compare_B {
+    if (( $A == $B )); then
+        echo "A = B"
+    elif (( $A > $B )); then
+        echo "A > B"
+    elif (( $A < $B )); then
+        echo "A < B"
+    fi
+}
+
+function B_compare_C {
+    if (( $B == $C )); then
+        echo "B = C"
+    elif (( $B > $C )); then
+        echo "B > C"
+    elif (( $B < $C )); then
+        echo "B < C"
+    fi
+}
+
+function A_compare_C {
+    if (( $A == $C )); then
+        echo "A = C"
+    elif (( $A > $C )); then
+        echo "A > C"
+    elif (( $A < $C )); then
+        echo "A < C"
+    fi
+}
+
 if [ -z $A ] || [ -z $B ] || [ -z $C ] || [[ ! $A == ?(-)+([0-9]) ]] || [[ ! $B == ?(-)+([0-9]) ]] || [[ ! $C == ?(-)+([0-9]) ]]; then
-    echo "The value of A, B, or C is empty or is not a number" 
-elif (( $A == $B )); then
-    if (( $B == $C )); then
-        echo "A = B = C"
-    elif (( $B > $C )); then
-        echo "A = B > C"
-    elif (( $B < $C )); then
-        echo "A = B < C"
-    fi
-elif (( $A > $B )); then
-    if (( $B == $C )); then
-        echo "A > B = C"
-    elif (( $B > $C )); then
-        echo "A > B > C"
-    elif (( $B < $C )); then
-        if (( $A == $C )); then
-            echo "B < A = C"
-        elif (( $A > $C )); then
-            echo "B < C < A"
-        elif (( $A > $B )); then
-            echo "B < A < C"
-        fi
-    fi
-elif (( $A < $B )); then
-    if (( $B == $C )); then
-        echo "A < B = C"
-    elif (( $B > $C )); then
-        if (( $A == $C )); then
-            echo "B > A = C"
-        elif (( $A < $C )); then
-            echo "A < C < B"
-        elif (( $A < $B )); then
-            echo "B > A > C"
-        fi
-    elif (( $B < $C )); then
-        echo "A < B < C"
-    fi
+    echo "The value of A, B, or C is empty or is not a number"
+    exit
 fi
+
+AB=$(A_compare_B)
+BC=$(B_compare_C)
+AC=$(A_compare_C)
+
+echo "$AB, $BC, $AC"
